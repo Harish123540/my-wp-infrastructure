06fc721fdcaf0a0ef4a30050d1a8b8764cf97779
Harish <harish@codefire.in>
2025-06-20 16:23:59 +0530

    sfdssfss

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,29 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const configPath = path.resolve(__dirname, '../config.json');
-const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+const config = {
+  projectName: process.env.PROJECT_NAME!,
+  aws: {
+    region: process.env.AWS_REGION!,
+    accountId: process.env.AWS_ACCOUNT_ID!,
+  },
+  github: {
+    owner: process.env.GITHUB_OWNER!,
+    infraRepo: process.env.GITHUB_INFRA_REPO!,
+    appRepo: process.env.GITHUB_APP_REPO!,
+    infraBranch: process.env.GITHUB_INFRA_BRANCH!,
+    appBranch: process.env.GITHUB_APP_BRANCH!,
+  },
+  docker: {
+    imageTag: process.env.DOCKER_IMAGE_TAG!,
+  },
+  ecs: {
+    containerPort: Number(process.env.ECS_CONTAINER_PORT),
+  },
+  rds: {
+    dbName: process.env.RDS_DB_NAME!,
+    dbUser: process.env.RDS_DB_USER!,
+  },
+};
 
 export default config;
--- lib/constructs/ecs/index.ts
+++ lib/constructs/ecs/index.ts
@@ -1,14 +1,24 @@
 import { Construct } from 'constructs';
+import { DeploymentControllerType } from 'aws-cdk-lib/aws-ecs';
+import * as path from 'path';
 import {
   Cluster,
   FargateTaskDefinition,
-  ContainerImage,
   LogDrivers,
   Secret,
   FargateService,
+  ContainerImage,
+  DeploymentCircuitBreaker,
 } from 'aws-cdk-lib/aws-ecs';
-import { Vpc, SecurityGroup, Peer, Port, SubnetType } from 'aws-cdk-lib/aws-ec2';
+import {
+  Vpc,
+  SecurityGroup,
+  Peer,
+  Port,
+  SubnetType,
+} from 'aws-cdk-lib/aws-ec2';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
 import { DatabaseInstance } from 'aws-cdk-lib/aws-rds';
 import { Duration } from 'aws-cdk-lib';
 import config from '../../config';
@@ -22,13 +32,17 @@ export class MyEcs extends Construct {
 
     const cluster = new Cluster(this, 'Cluster', { vpc });
 
+    const imageAsset = new DockerImageAsset(this, 'WordpressImage', {
+      directory: path.join(__dirname, '../../../../wordpress'),
+    });
+
     const taskDef = new FargateTaskDefinition(this, 'TaskDef', {
       cpu: 512,
       memoryLimitMiB: 1024,
     });
 
     taskDef.addContainer('WordpressContainer', {
-      image: ContainerImage.fromEcrRepository(ecrRepo, config.docker.imageTag),
+      image: ContainerImage.fromDockerImageAsset(imageAsset),
       portMappings: [{ containerPort: config.ecs.containerPort }],
       environment: {
         WORDPRESS_DB_HOST: dbInstance.dbInstanceEndpointAddress,
@@ -47,14 +61,11 @@ export class MyEcs extends Construct {
       logging: LogDrivers.awsLogs({ streamPrefix: 'wordpress' }),
     });
 
-    // ECS Security Group
     this.securityGroup = new SecurityGroup(this, 'ECSSecurityGroup', { vpc });
     this.securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80), 'Allow HTTP');
 
-    // Allow ECS to access RDS
     dbInstance.connections.allowDefaultPortFrom(this.securityGroup, 'Allow ECS to access RDS');
 
-    // Fargate Service
     this.fargateService = new FargateService(this, 'FargateService', {
       cluster,
       taskDefinition: taskDef,
@@ -62,6 +73,19 @@ export class MyEcs extends Construct {
       assignPublicIp: true,
       securityGroups: [this.securityGroup],
       vpcSubnets: { subnetType: SubnetType.PUBLIC },
+
+      // ✅ Correct property name
+      circuitBreaker: {
+        rollback: true,
+      },
+
+      // Optional - improve deployment behavior
+      deploymentController: {
+        type: DeploymentControllerType.ECS,
+      },
+
+      minHealthyPercent: 100,
+      maxHealthyPercent: 200,
     });
   }
 }
